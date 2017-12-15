46b41a74fb7224d0547deee0d0d8b6f1370bd141
dangoo <daniel.gooss@sinnerschrader.com>
2017-12-15 14:38:14 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+const express = require('express')
+const app = express()
+
+const credentials = {key: privateKey, cert: certificate};
+
+app.use('/static', express.static('static'))
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+//httpsServer.listen(443, () => console.log('Example app listening on port 443!'));
+app.listen(8080, () => console.log('Example app listening on port 8080!'))
