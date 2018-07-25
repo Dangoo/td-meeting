b2c8b8b948f9f4d8daaa9c701c30f35a2bb72539
dangoo <daniel.gooss@sinnerschrader.com>
2018-07-25 12:02:31 +0200

    Updates for schedule

--- _includes/program.md
+++ _includes/program.md
@@ -1,28 +1,28 @@
 
 <h2 class="headline">Das Programm</h2>
 
-<h3 class="headline">Freitag</h3>
+<h3 class="headline"><time datetime="2018-08-31">Freitag, 31.08.2018</time></h3>
 <ul class="singlecolumn">
   <li>Eintreffen der Teilnehmer des Treffens</li>
-  <li>Gem&uuml;tliches Beisammensein, Fachsimpeln und Vorträge (Abendessen nach Absprache) im Gruppenraum</li>
+  <li>Gemütliches Beisammensein, Fachsimpeln und Vorträge (Abendessen nach Absprache) im Gruppenraum (B)</li>
   </ul>
   
-<h3 class="headline">Samstag</h3>
+<h3 class="headline"><time datetime="2018-09-01">Samstag, 01.09.2018</time></h3>
 <ul class="singlecolumn">
-  <li>Etwa zweieinhalbst&uuml;ndige Fahrt mit dem <a href="http://www.hamburg.de/stadtrundfahrt/7844256/hafenbahn-fridolin/" target="_blank" title="Hafenbahn Fridolin">Triebwagen Fridolin</a> &uuml;ber die <a href="https://de.wikipedia.org/wiki/Hamburger_Hafenbahn" title="Hafenbahn Hamburg" target="_blank">Hamburger Hafenbahn</a></li>
-  <li>Besuch des Eisenbahnmuseums "<a href="http://www.vvm-museumsbahn.de/ix/ix-start/ix-start.php?id=300" title="Lokschuppen Aum&uuml;hle" target="_blank">Lokschuppen Aum&uuml;hle</a>" und vsl. Abendessen dort</li>
-  <li>Gem&uuml;tliches Beisammensein, Fachsimpeln und Vortr&auml;ge im Gruppenraum</li>
+  <li>Etwa zweieinhalbstündige Fahrt mit dem <a href="http://www.hamburg.de/stadtrundfahrt/7844256/hafenbahn-fridolin/" target="_blank" title="Hafenbahn Fridolin">Triebwagen Fridolin</a> (1) über die <a href="https://de.wikipedia.org/wiki/Hamburger_Hafenbahn" title="Hafenbahn Hamburg" target="_blank">Hamburger Hafenbahn</a></li>
+  <li>Besuch des Eisenbahnmuseums "<a href="http://www.vvm-museumsbahn.de/ix/ix-start/ix-start.php?id=300" title="Lokschuppen Aumühle" target="_blank">Lokschuppen Aumühle</a>" (2) und vsl. Abendessen dort</li>
+  <li>Gemütliches Beisammensein, Fachsimpeln und Vorträge im Gruppenraum</li>
 </ul>
 
-<h3 class="headline">Sonntag</h3>
+<h3 class="headline"><time datetime="2018-09-02">Sonntag, 02.09.2018</time></h3>
 <ul class="singlecolumn">
-<li>Fahrt mit dem Metronom von Hamburg nach L&uuml;neburg</li>
-<li>Fahrt mit dem <a href="http://heide-express.de/" target="_blank">Heide-Express</a> &uuml;ber die <a href="http://de.wikipedia.org/wiki/Bahnstrecke_L%C3%BCneburg%E2%80%93Bleckede" target="_blank">Strecke L&uuml;neburg-Bleckede</a> (Fahrzeug vsl. <a href="http://heide-express.de/index.php/GDT_0518.html" target="_blank">Triebwagen GTD 0518</a>, BJ 1955)</li>
-<li>R&uuml;ckfahrt mit dem Metronom nach Hamburg, nach Absprache ggf. Aufenthalt in L&uuml;neburg</li>
-<li>Zeit f&uuml;r weitere Aktivit&auml;ten, Abreise der Teilnehmer ohne Zusatztag</li>
+<li>Fahrt mit dem Metronom von Hamburg nach Lüneburg</li>
+<li>Fahrt mit dem <a href="http://heide-express.de/" target="_blank">Heide-Express</a> (3) über die <a href="http://de.wikipedia.org/wiki/Bahnstrecke_L%C3%BCneburg%E2%80%93Bleckede" target="_blank">Strecke Lüneburg-Bleckede</a> (Fahrzeug vsl. <a href="http://heide-express.de/index.php/GDT_0518.html" target="_blank">Triebwagen GTD 0518</a>, BJ 1955)</li>
+<li>Rückfahrt mit dem Metronom nach Hamburg, nach Absprache ggf. Aufenthalt in Lüneburg</li>
+<li>Zeit für weitere Aktivitäten, Abreise der Teilnehmer ohne Zusatztag</li>
 </ul>
 
-<h3 class="headline">Montag (optional)</h3>
+<h3 class="headline"><time datetime="2018-09-03">Montag, 03.09.2018</time> (optional)</h3>
 <ul class="singlecolumn">
-  <li>Besuch des <a href="https://www.miniatur-wunderland.de/" title="Miniatur Wunderland" target="_blank">Miniaturwunderlandes</a> mit F&uuml;hrung</li>
+  <li>Besuch des <a href="https://www.miniatur-wunderland.de/" title="Miniatur Wunderland" target="_blank">Miniaturwunderlandes</a> mit Führung (4)</li>
 </ul>
--- _site/static/index.js
+++ _site/static/index.js
@@ -1,59 +1,3 @@
-const photos = [
-    {
-        "height": 1365,
-        "photo_reference": "CmRaAAAANej8gm3Tl7TZHVI59LAD8RFFePLz3UB0We0WFIv08t924FAmD-r0JD-r_iQQwF0mHPmZbPx0zc62zcGYyMSkIo9wwjHNDUdOczaiIKkqCX8C_qMr521YW-_3cpxHSJIHEhCwfi2bsGzb4y4XiI52GYKPGhRaObnNtVBIpwggVWDVrV72MVfHAQ",
-        "width": 2048
-    },
-    {
-        "height": 1365,
-        "photo_reference": "CmRaAAAAV1lHKr9tY5Bmc7iglJqNbv_1_QSmxxeT2Y6Vi-ANhsKzEclq2EXB6gPeCBT8ZFZDRezkXML1cOKYixxrlC7o145kC2qq3vjbDSgLEHfAk6YdIHZUdhoSNwH-Ax9biS9IEhDrR-5SFUmt1zXpU1HHeJu-GhRoNlgr28-0bKRTER2w_Ld_vErWQQ",
-        "width": 2048
-    },
-    {
-        "height": 2048,
-        "html_attributions": [
-            "\u003ca href=\"https://maps.google.com/maps/contrib/108745540829695441635/photos\"\u003eAxel Kiefer\u003c/a\u003e"
-        ],
-        "photo_reference": "CmRaAAAAfXVpytQ_jD3C09vCMfHMhl04_d0Ixs0VtJ2_nmtZag3YZsze-ij9OKcEE9dFyhVPHHk3n23xCwc2e6ApfNzcyt0Nmw4lfE8IyEUaZXH1ig8uTD6YWrFJ2UBTf42557COEhAiMkGEm05NycrM6mqq40kBGhR7JeePie38IR-rqATZWCeLEHXgaQ",
-        "width": 1536
-    },
-    {
-        "height": 1377,
-        "photo_reference": "CmRaAAAAZaMJ90eZ62zuLr6Nk_S2L8cZN19W_ac5wlX5NpTglrpPgO9IbfzMpDZTU3UxCUlKxREbcdgHaK459zKh3ELC9C4A141tg1hUKTPDjAgL6Mo02SInOMnfs2GdIM2WRuM3EhCDJ6vkC0_R1nfqKIfeHAeBGhT59p0fD7_7fxiriqE8W9rpnDWddw",
-        "width": 2048
-    },
-    {
-        "height": 1408,
-        "photo_reference": "CmRaAAAAnl5uFMf8HHGk4c3S_xRjmeMGMcHqAXc5ddOxZo0aTwUN9_pu9msaGk9By2aQpV6th5l5J9br2ddV6r3CIQUYQnpgC007tW3ffN-lYTxKA6ZqzZGfxYIP6-tgOMjjsAFVEhA6u4cwALZKtwe4ws3EWxMOGhT8e8mPs9vl_OycYvs1Qi26C6ywKQ",
-        "width": 2048
-    },
-    {
-        "height": 1366,
-        "photo_reference": "CmRaAAAAA66SqjbUQ8hoDaDK5NNYXep6oiFEHfBzhLTe6AvQK8JQlfA0cGFUihhtsi2QlJlBtsIYnf-Fi9LF36pW8_FvXAxl2jqZQKLA39q7Od_McXaiFhXzq7-jm5N4pzIShHx1EhAHN-8TQ5SvfOweMBWcm6YsGhQAM2cV7ck7GYg10aTQIa4OnqxvBA",
-        "width": 2048
-    },
-    {
-        "height": 1365,
-        "photo_reference": "CmRaAAAA1M46lgBmHr6JJLmxikguwSq6m3Muf4umnS5qI0LDamKcr_GRHE5aa8J6PWB9MGdOz6lqa5nkcXwgNoTfLCd6idYMqFXvVtjFUX-Hk5GUaFTwGv3dV2G2XqygtNKTZaYJEhAv70XukZrDKOqE4oFWWqpIGhSbW1ZhCl2P_SIFjUxLmIXXmTi-DQ",
-        "width": 2048
-    },
-    {
-        "height": 1365,
-        "photo_reference": "CmRaAAAAQn7w3VEvnte7lvfUhqLkBkaU6AyK0jTMNSfJP4TPpWIsptp3m0ApQqUFK2V0boo0mT5QvN3s5XW5dVTIRj1exiGRo96Tke38TjG6nAbhesXqWR-bKAGi2wwooBGK7GcaEhDMUfwqFpZKoghvNsLfipPdGhQLyNBmDSMdCsjeDxp_jsyUvK-tMg",
-        "width": 2048
-    },
-    {
-        "height": 1365,
-        "photo_reference": "CmRaAAAA5kqt8mCZaK8sioeybbLZnyPJe-iBWCD1DjnPFD7EDKT9cqfm5mUk3LscptchCiGokzNFnBiqXu-HEY9EJ1vK7w_pUPzap5DSbjopVEZxTaRDvhA61bNLB9EAzZPtKBIIEhCQdBMT1ig73BYJSD3Pp_fdGhTCYY5zwxyvN3d6b91Gl29M6bONwg",
-        "width": 2048
-    },
-    {
-        "height": 1365,
-        "photo_reference": "CmRaAAAAnudVSZ-dANEQ4MUDGy5uECTwL0MRhsYYIHowhf8G75NifD4irJywI8VEAOajr4mN-EjHe5bw8tHcf5zStI30qV3pomlwOOo8sF7bEmpV2ipLVItWANc849z6tDtcn_c6EhAMmcfG3JUNcSUFLLz7SOgIGhR3K47h4MwcgZp_2NHTWTUrGDmlEw",
-        "width": 2048
-    }
-];
-
 function collectFotos(list) {
     const key = ' AIzaSyDn83rvM43t_4IZu6nwgTwV5ul_DtqYlfQ';
     const urls = list.map(function (item) {
@@ -99,7 +43,6 @@ function loadDeferedSrc(node) {
 
 function handleFirstLorySlide(e) {
     const items = e.target.querySelectorAll('[data-src]');
-    console.log(items);
     [].slice.call(items).forEach(loadDeferedSrc);
 
     e.target.removeEventListener('before.lory.slide', handleFirstLorySlide);
@@ -108,7 +51,7 @@ function handleFirstLorySlide(e) {
 const slider = document.querySelector('.js_slider');
 const sliderFrame = slider.querySelector('.js_frame');
 
-appendPhotos(sliderFrame);
+// appendPhotos(sliderFrame);
 
 document.addEventListener('DOMContentLoaded', function () {
     lory(slider, {
